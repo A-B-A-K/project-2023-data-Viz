ae5c84e8dbfdffca4a3cac622c8b7140cb1cbca4
Aristotelis <aristotelis.demetriou@gmail.com>
2023-05-05 23:08:19 +0200

    Help button + Top layer added

--- draft website/script.js
+++ draft website/script.js
@@ -11,32 +11,56 @@ document.addEventListener("DOMContentLoaded", function () {
     const button3 = document.querySelector("#button-3");
     const button4 = document.querySelector("#button-4");
     const button5 = document.querySelector("#button-5");
+    const resetButton = document.querySelector("#button-logo");
+
+    const helpButton = document.getElementById("button-help");
+    const helpWindow = document.getElementById("helpWindow");
+    const closeButton = document.getElementById("closeButton");
+
     let previousLayer;
 
     // Add click event listeners to the five buttons
     button1.addEventListener("click", function () {
         // Change the graph display to show statistics related to button 1
         updateGraph(1);
+        updateButton(1);
     });
 
     button2.addEventListener("click", function () {
         // Change the graph display to show statistics related to button 2
         updateGraph(2);
+        updateButton(2);
     });
 
     button3.addEventListener("click", function () {
         // Change the graph display to show statistics related to button 3
         updateGraph(3);
+        updateButton(3);
     });
 
     button4.addEventListener("click", function () {
         // Change the graph display to show statistics related to button 4
         updateGraph(4);
+        updateButton(4);
     });
 
     button5.addEventListener("click", function () {
         // Change the graph display to show statistics related to button 5
         updateGraph(5);
+        updateButton(5);
+    });
+
+    resetButton.addEventListener("click", function () {
+        updateGraph(-1);
+        updateButton(-1);
+    });
+
+    helpButton.addEventListener("click", function () {
+        helpWindow.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", function () {
+        helpWindow.style.display = "none";
     });
 
     //We want to create a function that will make it so that when the user has scrolled passed a threshold the buttons start to follow him
@@ -56,18 +80,38 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    function updateButton(buttonNumber) {
+        // Deselect all buttons
+        const buttons = document.querySelectorAll(".button-container button");
+        buttons.forEach(button => {
+            button.classList.remove("selected");
+        });
 
+        if (buttonNumber === -1) {
+            return;
+        }
+        // Select the clicked button if not already selected (if already selected, deselect all buttons)
+        const clickedButton = document.querySelector(`#button-${buttonNumber}`);
+        if (!clickedButton.classList.contains("selected")) {
+            clickedButton.classList.add("selected");
+        } else {
+            buttons.forEach(button => {
+                button.classList.remove("selected");
+            });
+        }
 
-    function updateGraph(buttonNumber) {
         const graph = document.querySelector("#graph");
         graph.classList.remove("fade-in");
         graph.textContent = `Graph for Button ${buttonNumber}`;
         graph.classList.add("fade-in");
+    }
+
+    function updateGraph(buttonNumber) {
 
         const textContainer = document.querySelector(".text-container");
         switch (buttonNumber) {
             case 1:
-                textContainer.innerHTML = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sollicitudin tortor sed ultrices ullamcorper. Nulla ultricies enim a quam tempus auctor.</p> <div class='graph1' style='border: 3px solid #3C3C3C; border-radius: 15px;'> </div>";
+                textContainer.innerHTML = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sollicitudin tortor sed ultrices ullamcorper. Nulla ultricies enim a quam tempus auctor.</p>`;
                 break;
             case 2:
                 textContainer.innerHTML = "<p>Ut suscipit, enim vitae maximus semper, turpis quam volutpat enim, id aliquet odio eros eget ante. Fusce finibus gravida sem, vitae hendrerit nulla iaculis a.</p>";
@@ -82,23 +126,44 @@ document.addEventListener("DOMContentLoaded", function () {
                 textContainer.innerHTML = "<p>Phasellus aliquet augue eget nibh pulvinar bibendum. Etiam finibus risus eu leo elementum suscipit. Donec rutrum elit eget faucibus placerat.</p>";
                 break;
             default:
-                textContainer.innerHTML = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sollicitudin tortor sed ultrices ullamcorper. Nulla ultricies enim a quam tempus auctor.</p>";
+                textContainer.innerHTML = `<p>Welcome to "Mapping the Shadows: An Interactive Journey Through Five Decades of Global
+                            Terrorism", an interactive data
+                            visualization project that invites you to explore the complex landscape of global terrorism
+                            from 1970 to 2020. By
+                            leveraging the power of the Global Terrorism Database(GTD), our goal is to provide an
+                            engaging and informative
+                            experience that sheds light on the trends, patterns, and impacts of terrorist activities
+                worldwide.< br >
+                    In this project, you will find a variety of visualizations, including histograms, heatmaps,
+                        and parallel coordinate
+                plots, all designed to present the data in a clear and accessible manner.At the heart of
+                            our project is an
+                            interactive map that adapts to your selections, allowing you to examine different aspects of
+                terrorism, such as the
+                            density of attacks, fatalities per country, and seasonality, among others.< br >
+                    We believe that understanding the historical context and trends of terrorism is essential
+                for
+                            fostering informed
+                            conversations and decision - making in our increasingly interconnected world.Please note that
+                this project deals with
+                            sensitive subject matter, and we have made every effort to present the information in an
+                            unbiased and respectful
+                manner.< br >
+                    To begin your exploration, simply scroll down and interact with the visualizations below.If
+                            you need help navigating
+                            the website or understanding the visualizations, you can access our help guide by clicking
+                            the button in the
+                top - right corner of the page.< br >
+                    We hope that this project provides you with valuable insights and encourages thoughtful
+                            reflection on the complex
+                            phenomenon of terrorism.</p > `;
         }
 
-        // Deselect all buttons
-        const buttons = document.querySelectorAll(".button-container button");
-        buttons.forEach(button => {
-            button.classList.remove("selected");
-        });
-
-        // Select the clicked button
-        const clickedButton = document.querySelector(`#button-${buttonNumber}`);
-        clickedButton.classList.add("selected", "hover");
     }
 
 
     // Define the map object and add it to the "map" div container
-    var mymap = L.map('map').setView([51.505, -0.09], 13);
+    var mymap = L.map('map').setView([46.5197, 6.6323], 13);
     // Add the tile layer to the map
     //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     //    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
